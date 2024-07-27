0d9ebb4ef5108960df3160fce662a7053c5faa42
rita <rita.elkhoury@icloud.com>
2024-07-27 16:43:52 +0300

    v2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert('rita')
+fetch('https://restcountries.com/v3.1/all')
+    .then(response=>response.json())
+    .then(json => {
+        json.forEach(element=> {
+        console.log(element.name.common);
+        let countries=document.getElementById('c')
+        let country=document.createElement('option')
+        country.innerHTML=element.name.common 
+        
+        countries.appendChild(country)
+
+
+        
+        });
+    
+    })
+
+   
